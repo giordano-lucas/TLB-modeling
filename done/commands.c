8b0915a8359887d23ab4b32bf5c33530fc8003d6
Lucas Giordano <giordano3102lucas@gmail.com>
2019-03-18 13:38:11 -0700

    add some methods to command.c

--- done/commands.c
+++ done/commands.c
@@ -20,8 +20,12 @@ int program_init(program_t* program){
 	
 	program->nb_lines = 0;
 	program->allocated = sizeof(program->listing);
+	
+	virt_addr_t v;
+	init_virt_addr(&v,0,0,0,0,0);
+	command_t command0 = {0,0,0,0,v}; 
 	for (int i = 0 ; i < program->allocated ; ++i){
-		command_t* c = &((program->listing)[i]);
+		/*command_t* c = &((program->listing)[i]);
 		c->order = 0;
 		c->type = 0;
 		c->data_size = 0;
@@ -29,7 +33,8 @@ int program_init(program_t* program){
 		virt_addr_t v;
 		init_virt_addr(&v,0,0,0,0,0);
 		c->vaddr = v;
-		
+		*/
+		(program->listing)[i] = command0;
 		
 	}
 	return ERR_NONE;
@@ -106,3 +111,74 @@ int program_print(FILE* output, const program_t* program){
 	return ERR_NONE;
 	}
 	
+/**
+ * @brief Tool function to down-reallocate memory to the minimal required size. Typically used once a program will no longer be extended.
+ * 
+ * Requirements
+ * 
+ * @param program : must be non null
+ */
+int program_shrink(program_t* program){
+	M_REQUIRE_NON_NULL(program);
+	return ERR_NONE;
+	}	
+	
+/**
+ * @brief add a command (line) to a program. Reallocate memory if necessary.
+ * 
+ * Requirements:
+ * @param program : must be non null
+ * @param command : must be non null
+ * 		+ according to command.types, data_size should be :
+ * 			- 1 or sizeof(word_t)for data
+ * 			- sizeof(word_t) for instructions
+ *		+ instruction and write cannot go together => can only read instructions
+ * 		+ vaddr.page_offset must be a multiple of data_size
+ * 
+ * Returns ERR_MEM if program already contains 100 commands
+ * 
+ */
+int program_add_command(program_t* program, const command_t* command){
+	M_REQUIRE_NON_NULL(program);
+	M_REQUIRE_NON_NULL(command);
+	//taille incorecte
+	 M_REQUIRE((command->type == INSTRUCTION)? (command->data_size == sizeof(word_t)): (command->data_size == 1 || command->data_size == sizeof(word_t)), ERR_SIZE, "Data size = %zu, type = %d, must be of size %d for Instructions and of size 1 or %d for Data", command->data_size, command->type, sizeof(word_t), sizeof(word_t));
+	//on cherche à écrire une instruction
+	 M_REQUIRE(!command->type == INSTRUCTION || command->order == WRITE, ERR_BAD_PARAMETER, "Cannot write with an instruction");
+	//addr virtuelle invalide
+	 M_REQUIRE((command->vaddr.page_offset % command->data_size == 0), ERR_ADDR, "Page Offset size = %" PRIu16 " must be a multiple of data size", command->vaddr.page_offset);
+	
+	if (program->allocated == program->nb_lines) {
+		return ERR_MEM;
+	}
+	else {
+		// à vérifier si on doit copier les pointeurs ou pas
+		program->listing[program->nb_lines] = *command;
+		program->nb_lines++;
+		return ERR_NONE;
+		}	
+	}
+	
+	
+//==================================== READ PART ===========================================
+
+command_t readCommand(FILE* input);
+int readUntilNextWhiteSpace(FILE* input){
+	M_REQUIRE_NON_NULL(input);
+	
+	}
+void handleRead(*command_t command);
+void handleWrite(*command_t command);
+/**
+ * @brief Read a program (list of commands) from a file.
+ * 
+ * Requirements:
+ * 
+ * @param filename : must be non null
+ * @param program : must be non null
+ */
+int program_read(const char* filename, program_t* program){
+	M_REQUIRE_NON_NULL(filename);
+	M_REQUIRE_NON_NULL(program);
+	return ERR_NONE;
+	}
