7d4b1279425934311def5cc9a090f107aa5e4d84
Léopaul <leopaul.boesinger@epfl.ch>
2019-04-13 08:47:53 +0200

    tlb functions

--- done/tlb.h
+++ done/tlb.h
@@ -18,3 +18,10 @@
  * Définir ici le type tlb_entry_t
  * (et supprimer ces quatre lignes de commentaire).
  */
+
+typedef struct{
+	uint64_t tag : VIRT_PAGE_NUM;
+	uint32_t phy_page_num : PHY_PAGE_NUM;
+	uint8_t v : 1;
+	
+} tlb_entry_t;
